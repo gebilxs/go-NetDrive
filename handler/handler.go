849cebd790acbce8b76962969d8186d56f96ff69
gebilxs <1799676669@qq.com>
2022-08-03 16:46:52 +0800

    add uploadfile

---
+++ handler/handler.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		//返回上传的html页面
+		data, err := ioutil.ReadFile("./static/view/index.html")
+		if err != nil {
+			io.WriteString(w, "internel server error")
+			return
+		}
+		io.WriteString(w, string(data))
+	} else if r.Method == "POST" {
+		//接受文件流即存储到本地目录
+	}
+}
