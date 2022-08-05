0adc4f839ed2813d08f788958e33c81bcf81de9e
gebilxs <1799676669@qq.com>
2022-08-05 22:41:34 +0800

    add mysql connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-NetDrive
 
 go 1.18
+
+require github.com/go-sql-driver/mysql v1.6.0
--- meta/filemeta.go
+++ meta/filemeta.go
@@ -1,6 +1,9 @@
 package meta
 
-import "sort"
+import (
+	mydb "go-NetDrive/db"
+	"sort"
+)
 
 //FileMeta : 文件元信息结构
 type FileMeta struct {
@@ -23,6 +26,12 @@ func UpdateFileMetas(fmeta FileMeta) {
 	return
 }
 
+//UpdateFileMetaDB新增更新元信息到mysql中
+func UpdateFileMetaDB(fmeta FileMeta) bool {
+	return mydb.OnFileUploadFinished(
+		fmeta.FileSha1, fmeta.FileName, fmeta.FileSize, fmeta.Location)
+}
+
 //GetFileMeta:通过Sha1 值获取文件的元信息对象
 func GetFileMeta(fileSha1 string) FileMeta {
 	return fileMetas[fileSha1]
@@ -39,6 +48,22 @@ func GetLastFileMetas(count int) []FileMeta {
 	return fMetaArray[0:count]
 }
 
+// RemoveFileMeta 删除相关的文件
 func RemoveFileMeta(fileSha1 string) {
 	delete(fileMetas, fileSha1)
+}
+
+//GetFilemetaDB :从sql获取文件元信息
+func GetFilemetaDB(fileSha1 string) (FileMeta, error) {
+	tfile, err := mydb.GetFileMeta(fileSha1)
+	if err != nil {
+		return FileMeta{}, nil
+	}
+	fmeta := FileMeta{
+		FileSha1: tfile.FileHash,
+		FileName: tfile.FileName.String,
+		FileSize: tfile.FileSize.Int64,
+		Location: tfile.FileAddr.String,
+	}
+	return fmeta, nil
 }
